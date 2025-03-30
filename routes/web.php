dbd837b26802fcb1f2c89276b357c5af53ea4a78
ajayitdeve <ajayitdev@gmail.com>
2025-03-30 15:25:04 +0530

    First Commit

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
+//test
 Auth::routes();
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
